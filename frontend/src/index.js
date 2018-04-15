a3140336c24b786c05dd1323acd57d526193994f
Tobias Laundal totokaka <tobias@laundal.no>
2018-04-15 18:00:20 +0200

    [frontend] Make the frontend usable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,3 @@
-function isPrime(number) {
-  number == 1;
-}
-
 class PrimeChecker {
   constructor(form, result) {
     this.form = form;
@@ -10,18 +6,35 @@ class PrimeChecker {
     this.form.addEventListener('submit', this.submit.bind(this));
   }
 
-  submit(event) {
+  async submit(event) {
     event.preventDefault();
 
-    isPrime(this.form[0].value);
+    this.result.innerHTML = 'Calculating...';
 
-    this.result.innerHTML = 'Submitted';
+    const number = this.form[0].value;
+    const res = await fetch(`/request/is-prime?num=${number}`);
+    if (!res.ok) {
+      this.result.innerHTML = 'Error while checking primality...';
+      return;
+    }
+
+    const body = await res.json();
+
+    if (body.type === 'result') {
+      this.result.innerHTML =
+        body.result ? `${number} is prime!` : `${number} is not prime.`;
+    } else if (body.type === 'enqueued'){
+      this.result.innerHTML = 'Still calculating...';
+      // TODO listen for result
+    } else {
+      this.result.innerHTML = 'The servers is behaving badly...';
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', function init() {
-  const form = document.getElementById('is-prime-form');
-  const result = document.getElementById('is-prime-result');
+  const form = document.getElementById('is_prime_form');
+  const result = document.getElementById('is_prime_result');
 
-  PrimeChecker(form, result);
+  new PrimeChecker(form, result);
 });
