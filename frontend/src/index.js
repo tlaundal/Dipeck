c4744ee2d311ec2071ae2b82b5d076dd35365a6e
Tobias Laundal totokaka <tobias@laundal.no>
2018-04-18 13:58:19 +0200

    [frontend] Test client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,69 +1,109 @@
-class PrimeChecker {
-  constructor(form, result) {
-    this.form = form;
-    this.result = result;
-
-    this.form.addEventListener('submit', this.submit.bind(this));
+async function queryIsPrime(number) {
+  const res = await fetch(`/request/is-prime?num=${number}`);
+  if (!res.ok) {
+    throw Error('Response not OK');
   }
 
-  async submit(event) {
-    event.preventDefault();
-
-    this.result.innerHTML = 'Calculating...';
-
-    const number = this.form[0].value;
-    const res = await fetch(`/request/is-prime?num=${number}`);
-    if (!res.ok) {
-      this.result.innerHTML = 'Error while checking primality...';
-      return;
-    }
-
-    const body = await res.json();
-
-    if (body.type === 'result') {
-      this.setResult(number, body.result);
-    } else if (body.type === 'enqueued'){
-      this.result.innerHTML = 'Still calculating...';
-      this.lookingFor = parseInt(number);
-      this.ensureListening();
-    } else {
-      this.result.innerHTML = 'The servers is behaving badly...';
-    }
+  const result = await res.json();
+  if (result.type === 'result') {
+    return {
+      type: 'result',
+      result: {
+        number,
+        isPrime: result.result
+      }
+    };
+  } else if (result.type === 'enqueued') {
+    return {
+      type: 'enqueued'
+    };
+  } else {
+    throw Error('Uknown response');
   }
+}
 
-  setResult(number, result) {
-    this.result.innerHTML =
-      result ? `${number} is prime!` : `${number} is not prime.`;
-  }
+class PrimeResultListener extends EventTarget {
+  listen(number, path=`ws://${window.location.hostname}/notification`) {
+    this.target = number;
 
-  ensureListening() {
     if (!this.socket || this.socket.readyState > 1) {
-      this.socket = new WebSocket(`ws://${window.location.hostname}/notification`);
+      this.socket = new WebSocket(path);
       this.socket.addEventListener('message', this.onMessage.bind(this));
-      this.socket.addEventListener('open', this.onOpen.bind(this));
+      this.socket.addEventListener('open', this.doHandshake.bind(this));
     } else {
-      this.onOpen();
+      this.doHandshake();
     }
   }
 
-  onOpen() {
-    this.socket.send(this.lookingFor.toString());
+  doHandshake() {
+    if (this.target || this.target === 0) {
+      this.socket.send(this.target.toString());
+    }
   }
 
   onMessage(message) {
     const parts = message.data.split(':');
-    if (parts.length !== 2 || parseInt(parts[0]) !== this.lookingFor) {
+    if (parts.length !== 2 || parseInt(parts[0]) !== this.target) {
       return;
     }
-
+    const isPrime = !!parseInt(parts[1]);
     this.socket.close();
-    this.setResult(this.lookingFor, !!parseInt(parts[1]));
+    this.dispatchEvent(new CustomEvent('result', {
+      detail: {
+        number: this.target,
+        isPrime
+      }
+    }));
+  }
+
+}
+
+class Dipeck {
+  constructor(input, target) {
+    this.input = input;
+    this.target = target;
+
+    this.primeResultListener = new PrimeResultListener();
+    this.primeResultListener.addEventListener('result',
+      e => this.done(e.detail));
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  status(message) {
+    this.target.innerHTML = message;
+  }
+
+  done(result) {
+    this.status(`${result.number} is ${result.isPrime ? 'prime' : 'not prime'}`);
+  }
+
+  async onSubmit(event) {
+    event.preventDefault();
+    const number = parseInt(this.input.value);
+
+    this.status('Calculating...');
+    const response = await queryIsPrime(number);
+
+    if (response.type === 'result') {
+      this.done(response.result);
+    } else if (response.type === 'enqueued') {
+      this.status('Still calculating...');
+      this.primeResultListener.listen(number);
+    }
   }
 }
 
+document.dipeck = {
+  Dipeck,
+  PrimeResultListener,
+  queryIsPrime
+};
 document.addEventListener('DOMContentLoaded', function init() {
   const form = document.getElementById('is_prime_form');
   const result = document.getElementById('is_prime_result');
 
-  new PrimeChecker(form, result);
+  const dipeck = new Dipeck(form[0], result);
+  form.addEventListener('submit', dipeck.onSubmit);
+  document.dispatchEvent(new Event('dipeck-loaded'));
 });
---
+++ frontend/test/test.js
@@ -0,0 +1,137 @@
+/* eslint-env mocha, node */
+const { JSDOM } = require('jsdom');
+const sinon = require('sinon');
+const assert = require('assert');
+const unroll = require('unroll');
+unroll.use(it);
+
+describe('frontend', function() {
+  let window;
+
+  before(function(callback) {
+    JSDOM.fromFile(`${__dirname}/../src/index.html`, {
+      runScripts: 'dangerously',
+      resources: 'usable'
+    }).then(dom => {
+      window = dom.window;
+
+      return new Promise(resolve => {
+        window.document.addEventListener('dipeck-loaded', resolve);
+      });
+    }).then(() => callback());
+  });
+
+  describe('#queryIsPrime()', function() {
+    before(function() {
+      window.fetch_original = window.fetch;
+    });
+    after(function() {
+      window.fetch = window.fetch_original;
+      delete window.fetch_original;
+    });
+    function fakeResponse(result, ok=true) {
+      return {
+        ok,
+        json: sinon.stub().resolves(result)
+      };
+    }
+
+    it('should fetch from server', async function() {
+      window.fetch = sinon.stub().resolves(fakeResponse({type:'enqueued'}));
+
+      await window.document.dipeck.queryIsPrime(13);
+
+      assert.ok(window.fetch.withArgs('/request/is-prime?num=13'));
+    });
+
+    unroll('should parse #type results', async function(data) {
+      window.fetch = sinon.stub().resolves(data.response);
+
+      const actual = await window.document.dipeck.queryIsPrime(134);
+
+      assert.deepEqual(actual, data.expected);
+    }, [
+      ['type', 'response', 'expected'],
+      ['result', fakeResponse({type:'result',result:false}), {type:'result',result:{number:134,isPrime:false}}],
+      ['enqueued', fakeResponse({type:'enqueued'}), {type:'enqueued'}]
+    ]);
+
+    unroll('should error on #type responses', async function(data) {
+      window.fetch = sinon.stub().resolves(data.response);
+
+      try {
+        await window.document.dipeck.queryIsPrime(134);
+        assert.fail();
+      } catch (e) {
+        // Success!
+      }
+    }, [
+      ['type', 'response'],
+      ['uknown', fakeResponse({type:'uknown'})],
+      ['not-ok', fakeResponse({type:'enqueued'},false)]
+    ]);
+  });
+
+  describe('PrimeResultListener', function() {
+    let primeResultListener;
+    beforeEach(function() {
+      primeResultListener = new window.document.dipeck.PrimeResultListener();
+      primeResultListener._document = window.document;
+      primeResultListener.socket = sinon.createStubInstance(window.WebSocket);
+    });
+
+    describe('#listen()', function() {
+      it('should attach open listener');
+      it('should attach message listener');
+      it('should reuse old connection if open');
+      it('should use correct path');
+    });
+
+    describe('#doHandshake()', function() {
+      it('should not send target when no target exists', function() {
+        primeResultListener.doHandshake();
+
+        assert.ok(primeResultListener.socket.send.notCalled);
+      });
+
+      unroll('should send target when target is #target', function(data) {
+        primeResultListener.target = data.target;
+        primeResultListener.doHandshake();
+
+        assert.ok(
+          primeResultListener.socket.send.withArgs(data.target.toString()).calledOnce);
+      }, [
+        ['target'],
+        [123],
+        [0]
+      ]);
+    });
+
+    describe('#onMessage()', function() {
+      it('should close the websocket on relevant message', function() {
+        primeResultListener.target = 123;
+        primeResultListener.onMessage({data:'123:0'});
+
+        assert.ok(primeResultListener.socket.close.calledOnce);
+      });
+      it('should not close the websocket on non-relevant message', function () {
+        primeResultListener.target = 123;
+        primeResultListener.onMessage({data:'113:1'});
+
+        assert.ok(primeResultListener.socket.close.notCalled);
+      });
+      it('should dispatch event for relevant message', async function() {
+        const eventPromise = new Promise(resolve => {
+          primeResultListener.addEventListener('result', resolve);
+        });
+
+        primeResultListener.target = 113;
+        primeResultListener.onMessage({data:'113:1'});
+
+        const {detail} = await eventPromise;
+
+        assert.deepEqual({number: 113, isPrime: true}, detail);
+      });
+    });
+  });
+});
