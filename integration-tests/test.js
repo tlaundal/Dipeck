eb22c43b0d9da9d75d8049d8db15a262c0c53b5f
Tobias <mail@totokaka.io>
2018-04-24 18:22:25 +0200

    [integration-tests] Full frontend tests using JSDOM

--- integration-tests/test.js
+++ integration-tests/test.js
@@ -1,22 +1,36 @@
 const assert = require('assert');
 const request = require('request-promise-native');
 const compose = require('docker-composer-manager');
+const { JSDOM } = require('jsdom');
 
 const composeFile = __dirname + '/../docker-compose.yml';
 let address;
 
-before(async function() {
-  this.timeout(0);
-  console.log('Starting docker.. May take a while');
-  await compose.dockerComposeUp(composeFile);
+function delay(t) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, t);
+  });
+}
+
+function log(msg) {
+  console.log(msg); // eslint-disable-line no-console
+}
+
+before(async function startDocker() {
+  this.timeout(60000);
+  log('Starting docker.. May take a while');
+  await compose.dockerComposeUp(composeFile, ['--renew-anon-volumes']);
+
   const port = await compose.dockerInspectPortOfContainer('dipeck_frontend_1');
   const host = (process.env.DOCKER_HOST || 'localhost').split(':')[0];
-  address = `${host}:${port}`
-  console.log('Docker is up');
-  console.log();
+  address = `${host}:${port}`;
+
+  log('Docker is up, waiting 10 extra seconds for services to settle');
+  await delay(12000);
+  log();
 });
 
-describe('frontend', function() {
+describe('dipeck', function() {
   it('should serve a webpage', async function() {
     const res = await request({
       uri: `http://${address}/`,
@@ -33,11 +47,63 @@ describe('frontend', function() {
 
     assert.equal(res.status, 'ok');
   });
+
+  describe('frontend', function() {
+    let window;
+    before(async function loadInJSDOM() {
+      // this.timeout(10);
+      const dom = await JSDOM.fromURL(`http://${address}/`, {
+        runScripts: 'dangerously',
+        resources: 'usable'
+      });
+      window = dom.window;
+
+      await new Promise(resolve => {
+        window.document.addEventListener('dipeck-loaded', resolve);
+      });
+    });
+
+    async function submit(number) {
+      const form = window.document.forms['is_prime_form'];
+      form.elements[0].value = number;
+      form.elements[1].click();
+
+      await new Promise(resolve => {
+        window.document.addEventListener('dipeck-result', resolve);
+      });
+    }
+
+    it('should find uncached non-primes', async function() {
+      this.timeout(5000);
+      await submit(8);
+      assert.equal('8 is not prime', window.document.querySelector('#is_prime_result').innerHTML);
+    });
+
+    it('should find cached non-primes', async function() {
+      this.timeout(5000);
+      await submit(8);
+      assert.equal('8 is not prime', window.document.querySelector('#is_prime_result').innerHTML);
+    });
+
+    it('should find uncached primes', async function() {
+      this.timeout(5000);
+      await submit(13);
+      assert.equal('13 is prime', window.document.querySelector('#is_prime_result').innerHTML);
+    });
+
+    it('should find cached primes', async function() {
+      this.timeout(5000);
+      await submit(13);
+      assert.equal('13 is prime', window.document.querySelector('#is_prime_result').innerHTML);
+    });
+
+
+  });
 });
 
-after(async function() {
-  this.timeout(0);
-  console.log('Stopping docker...');
+after(async function stopDocker() {
+  this.timeout(60000);
+  log('Stopping docker...');
   await compose.dockerComposeStop(composeFile);
-  console.log('Stopped');
+  log('Stopped');
 });
