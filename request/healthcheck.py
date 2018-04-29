97e821b9f5ea7b0690f0ea6a606d1328802c1e26
Tobias Laundal totokaka <tobias@laundal.no>
2018-04-29 23:14:29 +0200

    Fix stupid errors

--- request/healthcheck.py
+++ request/healthcheck.py
@@ -3,18 +3,20 @@ import sys
 import json
 from uwsgi_tools.curl import curl as ucurl
 
+
 def healthcheck():
     response = ucurl('127.0.0.1:5000', '/')
-    head, body = response.replace('\r\n', '\n').split('\n\n');
+    head, body = response.replace('\r\n', '\n').split('\n\n')
     body = json.loads(body)
     return '200 OK' in head and body['status'] == 'ok'
+
 
 if __name__ == '__main__':
     healthy = False
 
     try:
         healthy = healthcheck()
-    except:
+    except:  # noqa: F821
         pass
 
     sys.exit(0 if healthy else 1)
